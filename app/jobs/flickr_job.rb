ba9f2824728cfb53a182ef0ae7e2996473b0d74d
Horace Williams <horace.d.williams@gmail.com>
2012-06-25 10:01:16 -0400

    flickr job pulling basic results; ready to hook up to models when made

--- app/jobs/flickr_job.rb
+++ app/jobs/flickr_job.rb
@@ -10,25 +10,19 @@ class FlickrJob
     FlickRaw.api_key      = flickr_key
     FlickRaw.shared_secret= flickr_secret
 
-    place = flickr.places.findByLatLon(:lat => escape["latitude"], :lon => escape["longitude"], :accuracy => '6')
+    radius_photos = flickr.photos.search(:lat          => escape["latitude"],
+                                         :lon          => escape["longitude"],
+                                         :radius       => "20",
+                                         :radius_units => "mi",
+                                         :sort         => "interestingness-desc",
+                                         :extras       => "url_z") # include the medium-size image url in results
 
-    #photos = flickr.photos.search(:woe_id => place['woe_id'],
-                                  #:media  => 'photos',
-                                  #:sort   => 'interestingness-desc',
-                                  #:extras => 'url_z')
-
-
-    radius_photos = flickr.photos.search(:lat => escape["latitude"], :lon => escape["longitude"], :radius => "20", :radius_units => "mi", :sort => "interestingness-desc")
-    puts "Radial search found #{radius_photos.count} photos for #{escape["venue"]}!"
 
     query_string = escape["venue"] + " " + escape["state"]
-    keyword_photos = flickr.photos.search(:text => query_string, :sort => "interestingness-desc")
-    puts "Keyword search found #{keyword_photos.count} photos for #{escape["venue"]}!"
 
-    #photos.each do |photo|
-      #puts photo["url_z"]
-    #end
-
+    keyword_photos = flickr.photos.search(:text        => query_string,
+                                          :sort        => "interestingness-desc",
+                                          :extras      => "url_z")
   end
 end
 
