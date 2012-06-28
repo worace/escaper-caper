bfd4968f198a4ccb52b039e57555708d93c18345
Horace Williams <horace.d.williams@gmail.com>
2012-06-28 08:15:59 -0400

    after create hook to pull foursquare items

--- app/models/escape.rb
+++ app/models/escape.rb
@@ -3,13 +3,17 @@ class Escape < ActiveRecord::Base
                    :price, :phone, :street, :city, :state,
                    :zipcode, :expiration, :latitude, :longitude
 
-   after_create :pull_photos
+   after_create :pull_photos, :pull_attractions
 
    has_many :flickr_photos
    has_many :foursquare_spots
 
 
    def pull_photos
-    Resque.enqueue(FlickrJob, self.to_json)
+    Resque.enqueue(FlickrJob, self.to_json, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'])
+   end
+
+   def pull_attractions
+     Resque.enqueue(FoursquareJob, self.to_json, ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'])
    end
 end
