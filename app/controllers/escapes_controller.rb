6cf9bace1044f869fa7811b2c688c32e764d2a0c
Horace Williams <horace.d.williams@gmail.com>
2012-07-02 14:59:08 -0400

    in the midst of jankifying drop down

--- app/controllers/escapes_controller.rb
+++ app/controllers/escapes_controller.rb
@@ -4,6 +4,7 @@ class EscapesController < ApplicationController
   end
 
   def show
+    @categories = ParentCategory.all
     @escape = Escape.find_by_id(params[:id])
     @flickr_photos = @escape.flickr_photos
     @foursquare_spots = @escape.foursquare_spots
