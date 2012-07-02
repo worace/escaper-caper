db9fede63ed37dd0fd1089b35d78fb7a50e40143
Horace Williams <horace.d.williams@gmail.com>
2012-07-02 18:06:57 -0400

    adding display names for parent cats and price to escape show

--- app/models/parent_category.rb
+++ app/models/parent_category.rb
@@ -1,6 +1,21 @@
 class ParentCategory < ActiveRecord::Base
+  DISPLAY_NAMES = {"Food" => "Food & Dining",
+                   "Nightlife Spots" => "Bars & Nightlife",
+                   "Great Outdoors" => "Outdoors",
+                   "Travel & Transport" => "International",
+                   "Shops & Services" => "Shopping",
+                   "Professional & Other Places" => "City Living",
+                   "Colleges & Universities" => "College Life",
+                   "Arts & Entertainment" => "Arts & Entertainment",
+                   "Residences" => "Home Life"}
+
   attr_accessible :name, :display_name
   has_many :categories
 
+
   validates_presence_of :name
+
+  def display_name
+    DISPLAY_NAMES[name]
+  end
 end
