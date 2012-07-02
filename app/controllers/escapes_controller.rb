1b265046e6bb7df6bbff0dbff92e1dd52667e992
Horace Williams <horace.d.williams@gmail.com>
2012-07-02 09:35:18 -0400

    wodoafan dont know where im goingng

--- app/controllers/escapes_controller.rb
+++ app/controllers/escapes_controller.rb
@@ -7,5 +7,6 @@ class EscapesController < ApplicationController
     @escape = Escape.find_by_id(params[:id])
     @flickr_photos = @escape.flickr_photos
     @foursquare_spots = @escape.foursquare_spots
+    @items = (@flickr_photos + @foursquare_spots).shuffle
   end
 end
