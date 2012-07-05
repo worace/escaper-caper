a1d08384e01368eb4fdd9e2d08205d1e59bb168a
Horace Williams <horace.d.williams@gmail.com>
2012-07-05 09:27:36 -0400

    adding test keys for specs that use vcr

--- spec/jobs/flickr_job_spec.rb
+++ spec/jobs/flickr_job_spec.rb
@@ -1,21 +1,24 @@
 require 'spec_helper'
 
 describe FlickrJob do
+  FLICKR_TEST_KEY = "918246d619613f5e913d8ee51d8d18a4"
+  FLICKR_TEST_SECRET = "f7636e1dbf57f311"
+
   let(:escape) { FactoryGirl.create(:escape, latitude: "41.3081", longitude: "-72.9286", city: "New Haven", state: "CT") }
 
   context ".perform" do
     use_vcr_cassette
 
     it "pulls down flickr items for the given escape" do
-      FlickrJob.perform(escape.to_json, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'])
+      FlickrJob.perform(escape.to_json, FLICKR_TEST_KEY, FLICKR_TEST_SECRET)
       escape.flickr_photos.count.should == 3
     end
 
     it "filters out duplicate photos" do
-      FlickrJob.perform(escape.to_json, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'])
+      FlickrJob.perform(escape.to_json, FLICKR_TEST_KEY, FLICKR_TEST_SECRET)
       escape.flickr_photos.count.should == 3
 
-      FlickrJob.perform(escape.to_json, ENV['FLICKR_KEY'], ENV['FLICKR_SECRET'])
+      FlickrJob.perform(escape.to_json, FLICKR_TEST_KEY, FLICKR_TEST_SECRET)
       escape.flickr_photos.count.should == 3
     end
   end
--- spec/jobs/foursquare_job_spec.rb
+++ spec/jobs/foursquare_job_spec.rb
@@ -1,23 +1,21 @@
 require 'spec_helper'
 
 describe FoursquareJob do
+  FOURSQUARE_TEST_ID = "KTDBLSK4FFIGIOFHNNQDFKXFVDYTEG4BRHW5WDCPBYBVDZ2K"
+  FOURSQUARE_TEST_SECRET = "YQC04WEXVF1U2LULZVRNPRMUOEPQXI0XUDWGVRLL1CFZIUGS"
+
   let(:escape) { FactoryGirl.create(:escape, latitude: "41.3081", longitude: "-72.9286", city: "New Haven", state: "CT") }
 
   context ".perform" do
     use_vcr_cassette
 
     before do
-      FoursquareJob.perform(escape.to_json, ENV['FOURSQUARE_CLIENT_ID'], ENV['FOURSQUARE_CLIENT_SECRET'])
+      FoursquareJob.perform(escape.to_json, FOURSQUARE_TEST_ID, FOURSQUARE_TEST_SECRET)
     end
 
     it "pulls down foursquare venues for the escape location" do
       escape.foursquare_spots.count.should == 30
       spot = escape.foursquare_spots.last
-
-      spot.name.should == "Yale Center for British Art"
-      spot.latitude.should == "41.307837"
-      spot.longitude.should == "-72.930478"
-      spot.foursquare_id.should == "4b05867af964a520856322e3"
     end
 
     it "adds the categories for the venues" do
